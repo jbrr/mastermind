83e1c2f27b18e27c6c387342126a937da9f939ec
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-16 09:23:47 -0600

    Add quit path

--- mastermind.rb
+++ mastermind.rb
@@ -4,15 +4,26 @@ class MasterMind
 
     puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
-    choice = gets.chomp!.downcase
+    choice = gets.chomp.downcase
 
-    if choice = "p"
+    if choice == "p"
       play
-    elsif choice = "i"
+    elsif choice == "i"
       instructions
-    else
+    elsif choice == "q"
       quit
+    else
+      puts "Not a valid option, please press 'p', 'i', or 'q'"
     end
+  end
+
+  def instructions
+    puts "Try to guess the secret sequence of colors"
+    start
+  end
+
+  def quit
+    abort("Thanks for playing!")
   end
 end
 
