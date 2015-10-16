f24ab0ea34db52d25a92edbfca83d185d48cb554
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-16 09:18:14 -0600

    Add start method to display greeting

--- mastermind.rb
+++ mastermind.rb
@@ -1,2 +1,19 @@
 class MasterMind
+  def start
+    puts "Welcome to MASTERMIND"
+
+    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
+    choice = gets.chomp!.downcase
+
+    if choice = "p"
+      play
+    elsif choice = "i"
+      instructions
+    else
+      quit
+    end
+  end
 end
+
+MasterMind.new.start
