6084ab27ed9c66717bb27599785cde98d3d95554
Jeffrey Ruane <ruane.jb@gmail.com>
2015-10-16 09:27:05 -0600

    Add play intro message

--- mastermind.rb
+++ mastermind.rb
@@ -14,7 +14,22 @@ class MasterMind
       quit
     else
       puts "Not a valid option, please press 'p', 'i', or 'q'"
+      start
     end
+  end
+
+  def play_intro
+    puts "I have generated a beginner sequence with four elements made up of: (r)ed,
+(g)reen, (b)lue, and (y)ellow. Use (q)uit at any time to end the game."
+    puts "What's your guess?"
+  end
+
+  def secret_code
+    []
+  end
+
+  def play
+    play_intro
   end
 
   def instructions
